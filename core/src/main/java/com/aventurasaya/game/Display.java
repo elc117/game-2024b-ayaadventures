45d109ed81608cc9f24e89fe5838e883986171e1
Leonardo Dallanora <leonardo.dallanora@gmail.com>
2024-11-22 19:29:04 -0300

    criação da classe display para métodos de desenho repetitivos

---
+++ core/src/main/java/com/aventurasaya/game/Display.java
@@ -0,0 +1,31 @@
+package com.aventurasaya.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Display {
+
+    private Texture tHeart;
+    private Sprite heart;
+
+
+    public Display(SpriteBatch spriteBatch) {
+        tHeart = new Texture("heart.png");
+        heart = new Sprite(tHeart);
+        heart.setSize(heart.getWidth() /10f, heart.getHeight() / 10f);
+    }
+
+    public void desenhaVidas(Player p ,SpriteBatch spriteBatch) {
+        for (int i = 0; i < p.getVidas(); i++) {
+            float xOffset = i * (heart.getWidth() + 10); // Espaçamento de 10 pixels entre os corações
+            heart.setPosition(Main.WORLD_WIDTH - 60 - xOffset, // Começa no canto superior direito
+                Main.WORLD_HEIGHT - heart.getHeight() - 20); // Y fixo no topo
+            heart.draw(spriteBatch);
+        }
+    }
+
+    public void dispose() {
+        tHeart.dispose();
+    }
+}
--- core/src/main/java/com/aventurasaya/game/GameScreen.java
+++ core/src/main/java/com/aventurasaya/game/GameScreen.java
@@ -12,10 +12,9 @@ public class GameScreen implements Screen {
     private final Main game;
     private OrthographicCamera camera;
     private SpriteBatch spriteBatch;
-    private Texture backGround, tHeart;
-    private Sprite heart;
+    private Texture backGround;
     private Player p;
-
+    private Display display;
 
     public GameScreen(Main game) {
         this.game = game;
@@ -25,9 +24,8 @@ public class GameScreen implements Screen {
         this.backGround = new Texture("Game_Background.png");
         p = new Player(5);
 
-        tHeart = new Texture("heart.png");
-        heart = new Sprite(tHeart);
-        heart.setSize(heart.getWidth() /10f, heart.getHeight() / 10f);
+        display = new Display(spriteBatch);
+
     }
 
     @Override
@@ -38,17 +36,12 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
         spriteBatch.setProjectionMatrix(camera.combined);
-        game.getSpriteBatch().begin();
-        game.getSpriteBatch().draw(backGround, 0, 0, Main.WORLD_WIDTH, Main.WORLD_HEIGHT);
+        spriteBatch.begin();
+        spriteBatch.draw(backGround, 0, 0, Main.WORLD_WIDTH, Main.WORLD_HEIGHT);
 
-        for (int i = 0; i < p.getVidas(); i++) {
-            float xOffset = i * (heart.getWidth() + 10); // Espaçamento de 10 pixels entre os corações
-            heart.setPosition(Main.WORLD_WIDTH - 60 - xOffset, // Começa no canto superior direito
-                Main.WORLD_HEIGHT - heart.getHeight() - 40); // Y fixo no topo
-            heart.draw(game.getSpriteBatch());
-        }
+        display.desenhaVidas(p, spriteBatch);
 
-        game.getSpriteBatch().end();
+        spriteBatch.end();
     }
 
 
